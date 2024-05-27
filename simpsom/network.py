ac0a69d9237374730d695276d2b1eb7a2d61ed67
is-mat-tron <63111328+is-mat-tron@users.noreply.github.com>
2024-05-27 21:53:10 +0100

    Update network.py

--- simpsom/network.py
+++ simpsom/network.py
@@ -326,7 +326,6 @@ class SOMNet:
         self.learning_rate = self.start_learning_rate * \
                              self.xp.exp(-n_iter / self.epochs)
 
-    @nb.njit(parallel=True, fastmath=True)
     def find_bmu_ix(self, vecs: np.array) -> 'SOMNode':
         """Find the index of the best matching unit (BMU) for a given list of vectors.
 
